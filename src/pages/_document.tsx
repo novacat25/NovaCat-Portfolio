9172096ecce97127d6ede33069d40c49968744b6
NovaCat <sparkstorms2@gmail.com>
2024-11-01 12:44:14 +0900

    Feat: Header & Footer(only structures)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Footer } from "@/components/layouts/Footer/Footer"
+import { Header } from "@/components/layouts/Header/Header"
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Header />
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
   );
 }
