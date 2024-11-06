c3d36e01423897aa07d749d12b3f73ff7ba0e159
NovaCat <sparkstorms2@gmail.com>
2024-11-06 17:36:23 +0900

    Feat: Make GitHubIcon as an IconButton

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,14 +1,17 @@
-import { Box, Typography } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import React from 'react'
 import Link from 'next/link'
+import { colors } from '@/styles/colors'
 
 export const Footer = () => {
   const pc = () => (
-    <Box display="flex" gap={1} alignContent="center" justifySelf="right" sx={{ px: 7.5, py: 3 }}>
-      <Link href="https://github.com/novacat25" target="_blank">
-      <GitHubIcon />
-      </Link>
+    <Box display="flex" gap={1} alignItems="center" justifySelf="right" sx={{ px: 7.5, py: 3 }}>
+      <IconButton aria-label="github" size="small" sx={{ color: colors.text.primary }}>
+        <Link href="https://github.com/novacat25" target="_blank">
+          <GitHubIcon />
+        </Link>
+      </IconButton>
       <Typography>© NovaCat 2024 All rights reserved.</Typography>
     </Box>
   )
