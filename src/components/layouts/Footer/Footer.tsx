8ad0b0ecb182282a89be45957b1c53523527a58d
NovaCat <sparkstorms2@gmail.com>
2024-11-06 17:19:43 +0900

    Feat: make the footer component

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,9 +1,15 @@
 import { Box, Typography } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
 import React from 'react'
+import Link from 'next/link'
 
 export const Footer = () => {
+
   return (
-    <Box display="flex">
+    <Box display="flex" gap={1} alignContent="center" justifySelf="right" sx={{ px: 7.5, py: 3 }}>
+        <Link href="https://github.com/novacat25" target="_blank">
+         <GitHubIcon />
+        </Link>
       <Typography>© NovaCat 2024 All rights reserved.</Typography>
     </Box>
   )
