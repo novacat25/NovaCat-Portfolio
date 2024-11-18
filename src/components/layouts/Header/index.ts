9252f1f0a75457d664a36d501c294ee5cbddce13
NovaCat <sparkstorms2@gmail.com>
2024-11-18 16:26:35 +0900

    Refactoring

--- src/components/layouts/Header/index.ts
+++ src/components/layouts/Header/index.ts
@@ -1,4 +1,6 @@
+import { Header } from "./Header"
+
 import { PCHeaderLayout } from "./PC/PCHeaderLayout"
 import { SPHeaderLayout } from "./SP/SPHeaderLayout"
 
-export { PCHeaderLayout, SPHeaderLayout }
+export { PCHeaderLayout, SPHeaderLayout, Header }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css"
-import { Footer } from "@/components/layouts/Footer/Footer"
-import { Header } from "@/components/layouts/Header/Header"
+import { Footer } from "@/components/layouts/Footer"
+import { Header } from "@/components/layouts/Header"
 import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
