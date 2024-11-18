37ffcd289ccc16afab334e891f017e2972715fd7
NovaCat <sparkstorms2@gmail.com>
2024-11-18 10:23:04 +0900

    [WIP]Feat: page components

---
+++ src/components/pages/Introduce.tsx
@@ -0,0 +1,10 @@
+import { colors } from "@/styles/colors"
+import { Paper } from "@mui/material"
+
+export const IntroducePage = () => {
+  return (
+    <Paper elevation={0} sx={{ backgroundColor: colors.background.main, p: 2 }}>
+        An empty page.
+    </Paper>
+  )
+}
