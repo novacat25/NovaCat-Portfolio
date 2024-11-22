18cac431a458b690ff52de355e5303851c1279f5
NovaCat <sparkstorms2@gmail.com>
2024-11-22 17:05:19 +0900

    [WIP]Feat: PC layout on the contact page

--- src/components/pages/Contact/PC/PCContactPage.tsx
+++ src/components/pages/Contact/PC/PCContactPage.tsx
@@ -1,15 +1,16 @@
 import { H1Typography } from "@/components/atoms/Typography"
 import { colors } from "@/styles/colors"
-import { Paper, Stack } from "@mui/material"
+import { Send } from "@mui/icons-material"
+import { Button, Paper, Stack, TextField } from "@mui/material"
 
 export const PCContactPage = () => {
   return (
     <Paper elevation={0} sx={{ backgroundColor: colors.background.main, pt: 2, pl: 7, pb: 20 }}>
       <H1Typography fontWeight="bold" color={colors.primary.secondary}>Contact Me</H1Typography>
-      <Stack>
-        <p>title here</p>
-        <p>contents here</p>
-        <p>send button here</p>
+      <Stack sx={{ '& .MuiTextField-root': { my: 1, width: '25ch' } }}>
+        <TextField id="mail-title" label="Title" variant="outlined" sx={{ backgroundColor: colors.mono.white }} />
+        <TextField id="mail-content" label="Content" variant="outlined" multiline maxRows={5} sx={{ backgroundColor: colors.mono.white }} />
+        <Button startIcon={<Send />} variant="contained" sx={{ borderRadius: 12, py: 1.5, px: 2, width: '8%'}}>Send</Button>
       </Stack>
     </Paper>
   )
