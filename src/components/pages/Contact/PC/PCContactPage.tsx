196c5e100e2cb055d3b7fbc1e6f2e379c1ad6981
NovaCat <sparkstorms2@gmail.com>
2024-11-25 18:05:12 +0900

    [WIP]Feat: adding input fields

--- src/components/pages/Contact/PC/PCContactPage.tsx
+++ src/components/pages/Contact/PC/PCContactPage.tsx
@@ -7,9 +7,9 @@ export const PCContactPage = () => {
   return (
     <Paper elevation={0} sx={{ backgroundColor: colors.background.main, pt: 2, pl: 7, pb: 20 }}>
       <H1Typography fontWeight="bold" color={colors.primary.secondary}>Contact Me</H1Typography>
-      <Stack sx={{ '& .MuiTextField-root': { my: 1, width: '25ch' } }}>
-        <TextField id="mail-title" label="Title" variant="outlined" sx={{ backgroundColor: colors.mono.white }} />
-        <TextField id="mail-content" label="Content" variant="outlined" multiline maxRows={5} sx={{ backgroundColor: colors.mono.white }} />
+      <Stack sx={{ '& .MuiTextField-root': { my: 1 } }}>
+        <TextField id="mail-title" label="Title" variant="outlined" sx={{ width: "50%", backgroundColor: colors.mono.white }} />
+        <TextField id="mail-content" label="Content" variant="outlined" multiline rows={16} sx={{ pb:1, width: "80%", backgroundColor: colors.mono.white }} />
         <Button startIcon={<Send />} variant="contained" sx={{ borderRadius: 12, py: 1.5, px: 2, width: '8%'}}>Send</Button>
       </Stack>
     </Paper>
