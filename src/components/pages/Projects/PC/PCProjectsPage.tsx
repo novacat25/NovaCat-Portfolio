b722869257bc90f69230b0d2b27b5ca83956d6b5
NovaCat <sparkstorms2@gmail.com>
2024-11-26 16:42:53 +0900

    Feat: Project Card component

--- src/components/pages/Projects/PC/PCProjectsPage.tsx
+++ src/components/pages/Projects/PC/PCProjectsPage.tsx
@@ -1,11 +1,6 @@
 import { Paper } from "@mui/material"
 import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import { PROJECTS_CARD_SIZE } from "@/constants/size"
+import { ProjectCard } from "@/components/atoms/ProjectCard"
 
 const bull = (
   <Box
@@ -19,25 +14,7 @@ const bull = (
 export const PCProjectsPage = () => {
   return (
     <Paper elevation={0} sx={{ pt: 7.5 , px: 15, pb: 20 }}>
-      <Card sx={{ borderRadius: '24px', width: PROJECTS_CARD_SIZE, height: PROJECTS_CARD_SIZE }}>
-        <CardContent>
-          <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      <ProjectCard />
     </Paper>
     )
 }
--- src/components/pages/Projects/SP/SPProjectsPage.tsx
+++ src/components/pages/Projects/SP/SPProjectsPage.tsx
@@ -1,9 +1,10 @@
+import { ProjectCard } from "@/components/atoms/ProjectCard"
 import { Paper } from "@mui/material"
 
 export const SPProjectsPage = () => {
   return (
     <Paper elevation={0} sx={{ px: 2, pt: 2, pb: 6 }}>
-      projects
+      <ProjectCard isMobile />
     </Paper>
 )
 }
