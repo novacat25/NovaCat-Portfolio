53e32a39295141d021235f83ae5e27756b06b807
NovaCat <sparkstorms2@gmail.com>
2024-11-26 10:37:06 +0900

    Feat: projects page structure

---
+++ src/components/pages/Projects/PC/PCProjectsPage.tsx
@@ -0,0 +1,10 @@
+import { colors } from "@/styles/colors"
+import { Paper } from "@mui/material"
+
+export const PCProjectsPage = () => {
+  return (
+    <Paper elevation={0} sx={{ backgroundColor: colors.background.main, pt: 7.5 , px: 15, pb: 20 }}>
+      projects
+    </Paper>
+    )
+}
---
+++ src/components/pages/Projects/SP/SPProjectsPage.tsx
@@ -0,0 +1,10 @@
+import { colors } from "@/styles/colors"
+import { Paper } from "@mui/material"
+
+export const SPProjectsPage = () => {
+  return (
+    <Paper elevation={0} sx={{ backgroundColor: colors.background.main, px: 2, pt: 2, pb: 6 }}>
+      projects
+    </Paper>
+)
+}
