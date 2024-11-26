ce822d74db3dd9542ee654b107834d580b198789
NovaCat <sparkstorms2@gmail.com>
2024-11-26 16:45:14 +0900

    Fix: About page constants

--- src/components/pages/About/PC/PCAboutPage.tsx
+++ src/components/pages/About/PC/PCAboutPage.tsx
@@ -1,5 +1,5 @@
 import { AboutForum, AboutGreeting, AboutIntroduceMyselfCard } from "@/components/organisms/About"
-import { ABOUT_PC } from "@/constants/image_size"
+import { ABOUT_IMAGE_PC } from "@/constants/size"
 import { colors } from "@/styles/colors"
 import { Box, Paper, Skeleton } from "@mui/material"
 
@@ -8,7 +8,7 @@ export const PCAboutPage = () => {
     <Paper elevation={0} sx={{ backgroundColor: colors.background.main, pt: 7.5 , px: 15, pb: 20 }}>
       <Box display="flex" alignItems="center" gap={12}>
         <Box sx={{ width: '100%' }}>
-          <Skeleton variant="circular" width={ABOUT_PC} height={ABOUT_PC} />
+          <Skeleton variant="circular" width={ABOUT_IMAGE_PC} height={ABOUT_IMAGE_PC} />
         </Box>
         <Box>
           <AboutGreeting />
